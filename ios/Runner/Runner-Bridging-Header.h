b6b7ed154043347a219d8067e7d34bc718ecdcdf
egorp773 <episarenko773@gmail.com>
2025-12-17 17:38:47 +0300

    1

--- ios/Runner/Runner-Bridging-Header.h
+++ ios/Runner/Runner-Bridging-Header.h
@@ -1,1 +1,9 @@
-#import "GeneratedPluginRegistrant.h"
+//
+//  Runner-Bridging-Header.h
+//  Runner
+//
+
+#ifndef Runner_Bridging_Header_h
+#define Runner_Bridging_Header_h
+
+#endif /* Runner_Bridging_Header_h */
